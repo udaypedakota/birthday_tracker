c6102617b54816f6df462b1b776ea7d8b2feab0c
udaypedakota <peddakotaudaykumar@gmail.com>
2026-04-18 15:12:09 +0530

    rendor service added

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'src')));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
